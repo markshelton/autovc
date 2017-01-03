63e1afb52e7a40d348316ef708b6889b0b637932
Mark Shelton <mark@shelton.id.au>
2017-01-03 13:32:38 +0800

    changes to async requests

--- dev/scripts/main.py
+++ dev/scripts/main.py
@@ -9,16 +9,13 @@ import json
 import os
 import csv
 import time
-from functools import wraps
 import atexit
 
 #third-party modules
-import concurrent.futures as cf
-from requests_futures.sessions import FuturesSession
-from ratelimit import *
+from simple_requests import *
 
 #local modules
-import logManager as lm
+import logManager
 import dbLoader as db
 from configManager import configManager
 
@@ -51,21 +48,14 @@ cm = db.load_config()
 log = logging.getLogger(__name__)
 
 #requests
-session = FuturesSession(max_workers=int(cm.max_workers))
+#session = FuturesSession(max_workers=int(cm.max_workers))
 
-#functions
 
-def request_api(base_url, version, node_type, node_id, cb_key):
-    url = ("%s/%s/%s/%s?user_key=%s"
-           % (base_url, version, node_type, node_id, cb_key))
-    future = session.get(url)
-    return future
+#helper functions
 
-def load_node_list(archive_file, extract_dir, database_file, cb_key):
-    response = request_api("node_keys","node_keys.tar.gz",cb_key).result()
-    with open(archive_file, 'wb') as f:
-        f.write(response.content)
-    db.load_database(archive_file, extract_dir, database_file)
+def format_request(node_type, node_id):
+    request = "%s/%s/%s/%s?user_key=%s" % (cm.base_url, cm.version, node_type, node_id, cm.cb_key)
+    return request
 
 def parse_json(json):
     record = {}
@@ -74,77 +64,121 @@ def parse_json(json):
         record[field] = json["data"]["properties"][field]
     return record
 
-def store_temp(record, temp_file):
+def store_record(record, store):
     write_header = True
-    if os.path.isfile(temp_file):
+    if os.path.isfile(store):
         write_header = False
-    elif not os.path.exists(os.path.dirname(temp_file)):
-        os.makedirs(os.path.dirname(temp_file))
-    with open(temp_file, 'a+',newline='') as f:
+    elif not os.path.exists(os.path.dirname(store)):
+        os.makedirs(os.path.dirname(store))
+    with open(store, 'a+',newline='') as f:
         writer = csv.writer(f,delimiter=",")
         if write_header: writer.writerow(record.keys())
         writer.writerow(record.values())
 
-def track_time(start_time, current_record, total_records):
-        elapsed_time = time.time() - start_time
-        percent_complete = current_record / float(total_records)
-        time_remaining = (elapsed_time / percent_complete - elapsed_time)
-        log.info("%s | %s | %s | %.2f | %.2f | %.2f" % (table, current_record, total_records, percent_complete * 100, elapsed_time / 60, time_remaining / 60))
-
-def select_records(table, conn_db, conn_nl):
-    query = "SELECT %s FROM %s" % (TABLE_PK_LOOKUP[table],table)
-    #TODO more complicated diff
-    records = connection.execute(query).fetchall()
-    return records
-
-def make_request(table, record, cb_key):
-    future = request_api(table,record,cb_key)
-    return future
-
-def store_response(response, temp_file):
-    data = parse_json(response.json())
-    store_temp(data, temp_file)
-
-def load_temp(temp_file, database_file):
-    db.load_file(temp_file, database_file)
-
-def load_table(table, conn_db, conn_nl, extract_dir, cb_key):
-
-    records = select_records(connection, table)
-    current_record, total_records = 1, len(records)
-    start_time = time.time()
-    futures = {}
-    for record, in records:
-        future = make_request(API_TABLE_LOOKUP[table],record,cb_key)
-        futures[future] = record
-    temp_file = "%s%s.csv" % (extract_dir, table)
-    atexit.register(load_temp, temp_file, database_file)
-    for future in cf.as_completed(futures.keys(), timeout=cm.api_timeout):
-        try:
-            response = future.result()
-            store_response(response, temp_file)
-            track_time(start_time, current_record, total_records)
-        except Exception as e:
-            log.error("Error %s on %s/%s" % (e, table, futures[future]))
-        finally:
-            current_record += 1
-    load_temp(temp_file, database_file)
-
-def get_tables(database_file):
-    connnection = sqlite3.connect(database_file)
+def get_tables(database):
+    connnection = sqlite3.connect(database)
     query = "SELECT name FROM sqlite_master WHERE type=\'table\'"
     return connnection.execute(query)
 
-def load_data(nl_database_file, extract_dir, database_file, cb_key):
-    for table, in get_tables(nl_database_file):
-        load_table(table,conn_db,conn_nl,extract_dir,cb_key)
-    conn_nl.close()
+def get_nodelist():
+    request = format_request("node_keys","node_keys.tar.gz")
+    session = Requests()
+    print(request)
+    response = session.one(request)
+    with open(cm.nl_archive_file, 'wb') as f:
+        f.write(response.content)
+    return cm.nl_archive_file
+
+def track_time(start_time, current_record, total_records, table):
+    elapsed_time = time.time() - start_time
+    percent_complete = current_record / float(total_records) + 0.001
+    time_remaining = (elapsed_time / percent_complete - elapsed_time)
+    log.info("%s | %s | %s | %.2f | %.2f | %.2f" % (table, current_record, total_records, percent_complete * 100, elapsed_time / 60, time_remaining / 60))
+
+def store_response(response):
+    record = parse_json(response.json())
+    table = record["api_path"].split("/")[0]
+    store = "%s%s.csv" % (cm.crawl_extract_dir, table)
+    store_record(record, store)
+
+class preprocessor(ResponsePreprocessor):
+    def success(self, bundle):
+        store_response(bundle.response)
+        return bundle.ret()
+
+    def error(self, bundle):
+        #raise type(bundle.exception)(bundle.exception).with_traceback(bundle.traceback)
+        return None
+
+#core functions
+
+#Done
+def setup():
+    nodelist = load_nodelist()
+    database = cm.database_file
+    tables = get_tables(nodelist)
+    return nodelist, database, tables
+
+#Done
+def load_nodelist():
+    if not(os.path.isfile(cm.nl_database_file)):
+        nodelist = get_nodelist()
+        db.load_file(nodelist,cm.nl_database_file)
+    nodelist = cm.nl_database_file
+    return nodelist
+
+#TODO more complicated diff
+def select_records(nodelist, database, table):
+    connection = sqlite3.connect(nodelist)
+    query = "SELECT %s FROM %s" % (TABLE_PK_LOOKUP[table],table)
+    records = connection.execute(query).fetchall()
+    return records
+
+#Done
+def prepare_requests(records, table):
+    requests = [format_request(table, record) for record, in records]
+    return requests
+
+#Done
+def make_requests(requests):
+    session = Requests(concurrent=int(cm.max_workers),
+                   minSecondsBetweenRequests=60/float(cm.requests_per_min),
+                   defaultTimeout=int(cm.default_timeout),
+                   responsePreprocessor=preprocessor(),
+                   retryStrategy=Backoff())
+    session.swarm(requests)
+    """
+    while True:
+        print(len(session._requestQueue.queue))
+        if len(session._requestQueue.queue) == 0:
+            print("HELLO")
+            break
+    """
+
+#Done
+def load_responses(database, table):
+    store = "%s%s.csv" % (cm.crawl_extract_dir, table)
+    db.load_file(store, database)
 
 def main():
-    #cm = db.load_config()
-    #load_node_list(cm.nl_archive_file, cm.nl_extract_dir,cm.nl_database_file, cm.cb_key)
-    load_data(cm.nl_database_file, cm.crawl_extract_dir, cm.database_file, cm.cb_key)
+    nodelist, database, tables = setup()
+    for table, in tables:
+        records = select_records(nodelist, database, table)
+        requests = prepare_requests(records, table)
+        make_requests(requests)
+        #store_responses(responses, len(records), table)
+        #load_responses(database, table)
 
 if __name__ == "__main__":
     main()
 
+#Graveyard
+
+#Done
+def store_responses(responses, total, table):
+    start_time = time.time()
+    store = "%s%s.csv" % (cm.crawl_extract_dir, table)
+    for tally, response in enumerate(responses):
+        record = parse_json(response.json())
+        store_record(record, store)
+        track_time(start_time, tally, total, table)
