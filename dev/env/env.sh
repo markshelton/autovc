c99200ce4fca909c98d7147dd34200a62202d07f
Mark Shelton <mark@shelton.id.au>
2017-02-06 20:50:04 +0800

    more restructuring

--- dev/env/env.sh
+++
@@ -1,5 +0,0 @@
-export PATH="~/anaconda/bin:$PATH"
-conda env create -f "../env.yaml"
-source activate honours
-python -u $1
-echo "closing bash"
--- dev/package/__init__.py
+++ dev/package/__init__.py
@@ -1,5 +1,1 @@
-import sys
-sys.path.insert(0, "/dev/scripts/package")
 __all__ = ['configManager', 'dbLoader', 'logManager', 'sqlManager']
-
-from source_sql.main import main; main()
